96c7d84347277910f9204129e1cd5dac2f4cd1cf
shubham423 <jethurishubham@gmail.com>
2021-11-29 08:43:32 +0530

    udpdate operation

--- includes/Constants.php
+++ includes/Constants.php
@@ -12,3 +12,8 @@
     define('USER_EXISTS', 102);
     define('USER_FAILURE', 103); 
     define('USER_AUTHENTICATED', 201);
+
+    
+    define('PASSWORD_CHANGED', 301);
+    define('PASSWORD_DO_NOT_MATCH', 302);
+    define('PASSWORD_NOT_CHANGED', 303);
